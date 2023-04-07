693ecd037b494117021ba83b5003994619ac7a17
Betül Kara <51272636+betul-kara@users.noreply.github.com>
2023-04-07 23:41:40 +0300

    Added PlayerController.cs

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+    CharacterController controller;
+
+    private void Start()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 moveForward = transform.forward * verticalInput * moveSpeed;
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        Vector3 moveRight = transform.right * horizontalInput * moveSpeed;
+
+        Vector3 moveDirection = moveForward + moveRight;
+
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
